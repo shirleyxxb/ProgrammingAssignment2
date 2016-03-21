778fb6c19ac87fa2aa40a9be3f7b5ca33944071b
shirleyxxb <shirleyxxb@foxmail.com>
2016-03-21 15:30:03 +0800

    update, please review this version

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,7 @@
 ##This R files contains two functions, which is useful in inversing matrix(es) and cache the result 
-## the function "makeCacheMatrix" creates a list which could be referred to by the other function "CacheSolve"
-##functino "CacheSolve" can inverse a matrix for the first time and return the inversed matrix to an object for the second time.
 
-## Write a short comment describing this function
+
+## The function "makeCacheMatrix" creates a list which could be referred to by the other function "CacheSolve"
 
 makeCacheMatrix <- function(x = matrix()) {
 	     i <- NULL
@@ -20,7 +19,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This functino "CacheSolve" can inverse a matrix for the first time and return the inversed matrix to an object for the second time.
 
 cacheSolve <- function(x, ...) {
 	 i <- x$getinverse()
@@ -28,6 +27,7 @@ cacheSolve <- function(x, ...) {
                 message("getting cached data")
                 return(i)
         }
+        message("inverse is not in memory so the inverse (if exist) is gonna be computed")
         data <- x$get()
         i <- solve(data)
         x$setinverse(i)
